ec4ea6ca25ef5cc64f174e8c01515e4cf0cbed2e
juanvidal-ux <juan.vidal.claro@gmail.com>
2026-04-09 16:11:04 -0300

    Update API base URL to production

--- abc.js
+++ abc.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    const API_BASE_URL = "http://localhost:8080";
+    const API_BASE_URL = "https://backend-sistema-comprass.onrender.com";
     
     // --- ELEMENTOS DO DOM ---
     const filtroArea = document.getElementById('filtro-area-abc');
